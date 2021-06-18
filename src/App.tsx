5b4a27ce5a573b18593ca818a8ed71f3e04d4ab1
TonyInfinity <quachhengtony@gmail.com>
2021-06-18 15:56:15 +0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.global.css';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
